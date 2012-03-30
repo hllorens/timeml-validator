94d659d78e468914e57625f1e6644796607c3e80
Hector Llorens <hectorlm1983@gmail.com>
2012-03-30 19:03:51 +0200

    first commit

---
+++ TimeML-validator-1.0-src/src/timemlvalidator/Main.java
@@ -0,0 +1,125 @@
+/*
+  Copyright 2012 Hector Llorens
+
+   Licensed under the Apache License, Version 2.0;
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package timemlvalidator;
+
+import java.io.*;
+import utils_bk.FileUtils;
+import nlp_files.XMLFile;
+import org.apache.commons.cli.*;
+
+/**
+ *
+ * @author Hector Llorens
+ * @since 2012
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String type = "tml"; // normal
+        String input_files[];
+
+        try {
+
+
+            Options opt = new Options();
+            //addOption(String opt, boolean hasArg, String description)
+            opt.addOption("h", "help", false, "Print this help");
+            opt.addOption("t", "type", true, "Type of validation: normal (default) or minimum");
+            opt.addOption("d", "debug", false, "Debug mode: Output errors stack trace (default: disabled)");
+
+            PosixParser parser = new PosixParser();
+            CommandLine cl_options = parser.parse(opt, args);
+            HelpFormatter hf = new HelpFormatter();
+            if (cl_options.hasOption('h')) {
+                hf.printHelp("TimeML-validator", opt);
+                System.exit(0);
+            } else {
+                if (cl_options.hasOption('d')) {
+                    System.setProperty("DEBUG", "true");
+                }
+
+                if (cl_options.hasOption('t')) {
+                    type = cl_options.getOptionValue("t");
+                    if (!type.equalsIgnoreCase("normal") && !type.equalsIgnoreCase("minimum")) {
+                        throw new Exception("Type (-t) must be either 'normal' or 'minimum'.");
+                    }
+                }
+
+                if(type.equalsIgnoreCase("minimum")){
+                    type="tml-min-consistency";
+                }else{
+                    type="tml";
+                }
+
+                input_files = cl_options.getArgs();
+
+                for (int i = 0; i < input_files.length; i++) {
+                    File f = new File(input_files[i]);
+                    if (!f.exists()) {
+                        throw new FileNotFoundException("File does not exist: " + f);
+                    }
+
+                    if (f.isFile()) {
+                        File[] files = {f};
+                        XMLFile xmlfile = new XMLFile();
+                        xmlfile.loadFile(f);
+                        xmlfile.overrideExtension(type);
+                        if (!xmlfile.isWellFormed()) {
+                            if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
+                                throw new Exception("File: " + xmlfile.getFile().getCanonicalPath() + " is not a valid TimeML XML file.");
+                            } else {
+                                System.out.println("File: " + xmlfile.getFile().getCanonicalPath() + " is not a valid TimeML XML file.");
+                            }
+                        }
+                    } else {
+                        File[] files = f.listFiles(FileUtils.onlyFilesFilter);
+                        if (files.length == 0) {
+                            throw new Exception("Empty folder: " + f.getName());
+                        }
+                        for (int fn = 0; fn < files.length; fn++) {
+                            XMLFile xmlfile = new XMLFile();
+                            xmlfile.loadFile(files[fn]);
+                            xmlfile.overrideExtension(type);
+                            if (!xmlfile.isWellFormed()) {
+                                if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
+                                    throw new Exception("File: " + xmlfile.getFile().getCanonicalPath() + " is not a valid TimeML XML file.");
+                                } else {
+                                    System.out.println("File: " + xmlfile.getFile().getCanonicalPath() + " is not a valid TimeML XML file.");
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+
+
+            }
+            System.out.println("All the files are valid!");
+        } catch (Exception e) {
+            System.err.println("Errors found:\n\t" + e.getMessage() + "\n");
+            if (System.getProperty("DEBUG") != null && System.getProperty("DEBUG").equalsIgnoreCase("true")) {
+                e.printStackTrace(System.err);
+            }
+            System.exit(1);
+        }
+
+    }
+}
